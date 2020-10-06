d76bee56c283f8ed11f2ef910bce9c5f5be100de
Benson1198 <bensonpx1198@gmail.com>
2020-10-06 17:09:37 +0530

    Coin Change DP

--- Dynamic Programming/Coin Change Count Combination.cpp
+++ Dynamic Programming/Coin Change Count Combination.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 // Simple Recursive solution
-int getCount(int coins[],int n, int sum){
+int getCountRec(int coins[],int n, int sum){
 
     if(sum == 0){
         return 1;
@@ -20,14 +20,44 @@ int getCount(int coins[],int n, int sum){
         return 0;
     }
 
-    int res = getCount(coins,n-1,sum);
+    int res = getCountRec(coins,n-1,sum);
 
     if(coins[n-1] <= sum){
-        res = res + getCount(coins,n,sum-coins[n-1]);
+        res = res + getCountRec(coins,n,sum-coins[n-1]);
     }
 
     return res;
 }
+
+
+
+
+// Dynamic Programming Approach
+int getCount(int coins[],int n,int sum){
+    int dp[sum+1][n+1];
+
+    for(int i=0;i<=n;i++){
+        dp[0][i] = 1;
+    }
+
+    for(int i=0;i<=sum;i++){
+        dp[i][0] = 1;
+    }
+
+    for(int i=1;i<= sum;i++){
+        for(int j=1;j<=n;j++){
+            dp[i][j] = dp[i][j-1];
+
+            if(coins[j-1] <= i){
+                dp[i][j] += dp[i-coins[j-1]][i];
+            }
+        }
+    }
+
+    return dp[sum][n];
+}
+
+
 
 int main(){
 
