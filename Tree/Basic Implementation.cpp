d895f44d1e3d0e90d543e8908a71c22165649836
Benson1198 <bensonpx1198@gmail.com>
2020-10-15 10:16:49 +0530

    Binary Tree Struct

--- Tree/Basic Implementation.cpp
+++ Tree/Basic Implementation.cpp
@@ -1,3 +1,19 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int k){
+        data = k;
+        left = NULL;
+        right = NULL;
+    }
+}
+
+int main(){
+    Node *root = new Node(10);
+}
