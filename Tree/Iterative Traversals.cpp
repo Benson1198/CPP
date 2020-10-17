f53bd429c23e9d4f458aabc8ec0656eef091753c
Benson1198 <bensonpx1198@gmail.com>
2020-10-17 13:47:10 +0530

    iterative Traversals

---
+++ Tree/Iterative Traversals.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int k){
+        data = k;
+        left = NULL;
+        right = NULL;
+    }
+}
+
+
+
+
+int main(){
+
+}
