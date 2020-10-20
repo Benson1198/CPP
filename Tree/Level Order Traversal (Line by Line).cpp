e43078286d846def36976535881e544ed6574bf8
Benson1198 <bensonpx1198@gmail.com>
2020-10-20 12:52:30 +0530

    LevelOrder(LinebyLine)

--- Tree/Level Order Traversal (Line by Line).cpp
+++ Tree/Level Order Traversal (Line by Line).cpp
@@ -14,7 +14,10 @@ struct Node{
     }
 }
 
-void levelorderLBL(Node *root){
+
+// Method 1 (By pushing a NULL into the queue after completing every level)
+
+void levelorderLBL1(Node *root){
     if(root == NULL){
         return;
     }
@@ -42,5 +45,38 @@ void levelorderLBL(Node *root){
         if(curr->right != NULL){
             q.push(curr->right);
         }
+    }
+}
+
+
+// Method 2 (By running two loops)
+
+void levelorderLBL2(Node *root){
+    if(root == NULL){
+        return;
+    }
+
+    queue <Node*> q;
+    q.push(root);
+
+    while(q.empty() == false){
+        int count = q.size();
+
+        for(int i=0;i<count;i++){
+            Node *curr = q.top();
+            q.pop();
+
+            cout << curr->data << " ";
+
+            if(curr->left != NULL){
+                q.push(curr->left);
+            }
+        
+            if(curr->right != NULL){
+                q.push(curr->right);
+            }
+        }
+
+        cout << "\n";
     }
 }
