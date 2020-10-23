f5d993021823a91cee9fdfff4e9768e1930bdfc9
Benson1198 <bensonpx1198@gmail.com>
2020-10-23 12:52:18 +0530

    LCA 2

--- Tree/Lowest Common Ancestor.cpp
+++ Tree/Lowest Common Ancestor.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 struct Node{
-    int data;
+    int key;
     Node *left;
     Node *right;
 
     Node(int k){
-        data = k;
+        key = k;
         left = NULL;
         right = NULL;
     }
@@ -16,6 +16,41 @@ struct Node{
 
 
 // Method 1 - Naive Approach
+bool findPath(Node *root, vector<int> &path, int k) 
+{ 
+
+    if (root == NULL) return false; 
+  
+    path.push_back(root->key); 
+  
+    if (root->key == k) 
+        return true; 
+  
+    if ( (root->left && findPath(root->left, path, k)) || 
+         (root->right && findPath(root->right, path, k)) ) 
+        return true; 
+
+    path.pop_back(); 
+    return false; 
+} 
+  
+
+int findLCA(Node *root, int n1, int n2) 
+{ 
+
+    vector<int> path1, path2; 
+  
+ 
+    if ( !findPath(root, path1, n1) || !findPath(root, path2, n2)) 
+          return -1; 
+  
+
+    int i; 
+    for (i = 0; i < path1.size() && i < path2.size() ; i++) 
+        if (path1[i] != path2[i]) 
+            break; 
+    return path1[i-1]; 
+} 
 
 
 
