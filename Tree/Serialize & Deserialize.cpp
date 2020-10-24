45ac3e280ed033e6c437c286d9b81002541b8b6f
Benson1198 <bensonpx1198@gmail.com>
2020-10-24 19:20:16 +0530

    Deserialize

--- Tree/Serialize & Deserialize.cpp
+++ Tree/Serialize & Deserialize.cpp
@@ -29,3 +29,24 @@ void serialize(Node *root,vector<int> &arr){
     serialize(root->left,arr);
     serialize(root->right,arr);
 }
+
+
+// Method to Deserialize a Binary Tree
+
+const int EMPTY = -1;
+int index = 0;
+
+Node *deserialize(vector<int> &arr){
+    if(index == arr.size()) return NULL;
+
+    int val = arr[index];
+    index++;
+
+    if(val == EMPTY) return NULL;
+
+    Node *root = newNode(val);
+    root->left = deserialize(arr);
+    root->right = deserialize(arr);
+
+    return root;
+}
