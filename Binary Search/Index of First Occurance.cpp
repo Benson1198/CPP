b0cd044b3a7736e901f6c075df107ae7e4c29db3
Benson1198 <bensonpx1198@gmail.com>
2020-10-29 19:28:51 +0530

    Searching

--- Binary Search/Index of First Occurance.cpp
+++ Binary Search/Index of First Occurance.cpp
@@ -1,3 +1,29 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
+int firstOcr(int arr[],int n,int x){
+    int low=0, high=n-1;
+
+    while(low<=high){
+        int mid = (high-low)/2;
+
+        if(arr[mid] > x){
+            high =  mid-1;
+        }
+
+        else if(arr[mid] < x){
+            low = mid+1;
+        }
+
+        else{
+            if(mid == 0 || arr[mid-1] != arr[mid]){
+                return mid;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+    }
+    return -1;
+}
