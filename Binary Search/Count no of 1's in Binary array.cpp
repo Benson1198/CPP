c6be4c9aa388ac4abf1696ce64665db776de4f6b
Benson1198 <bensonpx1198@gmail.com>
2020-10-30 09:44:14 +0530

    Count 1's in Binary Array

---
+++ Binary Search/Count no of 1's in Binary array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int countOnes(int arr[],int n){
+    int low=0, high=n-1;
+
+    while(low<=high){
+        int mid = (high-low)/2;
+
+        if(arr[mid] == 0){
+            low =  mid+1;
+        }
+
+        else{
+            if(mid == 0 || arr[mid-1] == 0){
+                return (n-mid);
+            }
+            else{
+                high = mid-1;
+            }
+        }
+    }
+    return 0;
+}
