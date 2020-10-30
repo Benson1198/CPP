4531ed73dca89e5536c27813db409adedd3c9441
Benson1198 <bensonpx1198@gmail.com>
2020-10-30 11:26:16 +0530

    BS Squr_root

---
+++ Binary Search/Square Root floor.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int sqRootFloor(int n){
+    int low =1,high =n,ans= -1;
+
+    while(low <= high){
+        int mid = (high-low)/2;
+        int mSq = mid*mid;
+
+        if(mSq == x){
+            return mid;
+        }
+        else if(mSq > x){
+            high = mid-1;
+        }
+
+        else{
+            low = mid-1;
+            ans = mid;
+        }
+    } 
+
+    return ans;
+}
