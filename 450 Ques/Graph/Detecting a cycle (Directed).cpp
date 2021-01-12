adae89749eb2047a84b3871a94d70f12d0c9af4c
Benson1198 <bensonpx1198@gmail.com>
2021-01-12 16:28:18 +0530

    +Detect cycle using Kahn's Algo

--- 450 Ques/Graph/Detecting a cycle (Directed).cpp
+++ 450 Ques/Graph/Detecting a cycle (Directed).cpp
@@ -39,3 +39,42 @@ bool detectCycle(vector<int> adj[], int v){
 
     return false;
 }
+
+
+
+
+
+// Using Kahn's Algorithm (Modified by adding a variable named count)
+
+
+void topologicalSort(vector<int> adj[], int V) 
+{ 
+    vector<int> in_degree(V, 0); 
+  
+    for (int u = 0; u < V; u++) { 
+        for (int x:adj[u]) 
+            in_degree[x]++; 
+    } 
+  
+    queue<int> q; 
+    for (int i = 0; i < V; i++) 
+        if (in_degree[i] == 0) 
+            q.push(i); 
+
+    int count=0;  
+    while (!q.empty()) { 
+        int u = q.front(); 
+        q.pop(); 
+  
+        for (int x: adj[u]) 
+            if (--in_degree[x] == 0) 
+                q.push(x); 
+        count++;
+    } 
+    if (count != V) { 
+        cout << "There exists a cycle in the graph\n"; 
+    }
+    else{
+        cout << "There exists no cycle in the graph\n";
+    }
+}
