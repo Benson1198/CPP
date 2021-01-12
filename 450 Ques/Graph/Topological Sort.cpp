c01cae896ff4dfd117dfa35b167535493724325a
Benson1198 <bensonpx1198@gmail.com>
2021-01-12 16:27:37 +0530

    Topo Sort Using DFS

--- 450 Ques/Graph/Topological Sort.cpp
+++ 450 Ques/Graph/Topological Sort.cpp
@@ -33,3 +33,43 @@ void topological_sort(vector<int> adj[], int v){
 }
 
 
+
+// DFS based solution(usning stack data structure)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void DFSutil(vector<int> adj[],int s,bool visited[], stack<int> &st){
+    visited[s] = true;
+
+    for(int x: adj[s]){
+        if(visited[x] == false){
+            DFSutil(adj,x,visited);
+        }
+
+        st.push(x);
+    }
+}
+
+void DFS(vector<int> adj[], int v){
+    bool visited[v+1];
+
+    stack<int> st;
+
+    for(int i=0;i<v;i++){
+        visited[i] = false;
+    }
+
+    for(int i=0;i<v;i++){
+        if(visited[i] == false){
+            DFSutil(adj,i,visited,st);
+        }
+    }
+
+    while(st.empty() == false){
+        int x = st.top();
+        cout << x << " ";
+
+        st.pop();
+    }
+}
