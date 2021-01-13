29d4174b99b0c7d66321165a089079090c97cb1c
Benson1198 <bensonpx1198@gmail.com>
2021-01-13 12:41:33 +0530

    CF 1

--- Codeforces/186A.cpp
+++ Codeforces/186A.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-
-
 int main(){
     string g1, g2;
 
@@ -15,106 +13,34 @@ int main(){
     n1 = g1.length();
     n2 = g2.length();
 
-    unordered_map <char,int> M;
-    bool ans = false;
-// if n1 is greater than n2
-    if(n1 > n2){
-        for(int i=0;i<n1;i++){
-            if(M.find(g1[i]) == M.end()){
-                M.insert(make_pair{g1[i],1});
-            }
+    int mismatch = 0;
+    vector<int> pos;
 
-            else{
-                M[g1[i]]++;
-            }
-        }
-
-        for(int i=0;i<n2;i++){
-            if(M.find(g2[i]) == M.end()){
-                continue;
-            }
-
-            else{
-                if(M[g2[i]]<0){
-                    break;
-                }
-                else{
-                    M[g2[i]]--;
-                }
-                
-            }
-        }
-        ans = true;
-    }
-
-// if n2 is greater than n1
-
-    else if(n2 > n1){
-        for(int i=0;i<n2;i++){
-            if(M.find(g2[i]) == M.end()){
-                M.insert(make_pair{g2[i],1});
-            }
-
-            else{
-                M[g2[i]]++;
-            }
-        }
-
-        for(int i=0;i<n1;i++){
-            if(M.find(g1[i]) == M.end()){
-                continue;
-            }
-
-            else{
-                if(M[g1[i]]<0){
-                    break;
-                }
-                else{
-                    M[g1[i]]--;
-                }
-                
-            }
-        }
-        ans = true;
+    if(n1 != n2){
+        cout << "NO" << endl;
     }
 
     else{
         for(int i=0;i<n1;i++){
-            if(M.find(g1[i]) == M.end()){
-                M.insert(make_pair{g1[i],1});
-            }
-
-            else{
-                M[g1[i]]++;
+            if(g1[i] != g2[i]){
+                mismatch++;
+                pos.push_back(i);
             }
         }
 
-        for(int i=0;i<n2;i++){
-            if(M.find(g2[i]) == M.end()){
-                break;
+        if(mismatch != 2){
+            cout << "NO" << endl;
+        }
+
+        else{
+            if(g1[pos[0]] == g2[pos[1]] && g2[pos[0]] == g1[pos[1]]){
+                cout << "YES" << endl;
             }
 
             else{
-                if(M[g2[i]]<0){
-                    break;
-                }
-                else{
-                    M[g2[i]]--;
-                }
-                
+                cout << "NO" << endl;
             }
-
-            ans = true;
         }
     }
-
-    if(ans){
-        cout << "YES" << endl;
-    }
-    else{
-        cout << "NO"<< endl;
-    }
-
-
     return 0;
 }
