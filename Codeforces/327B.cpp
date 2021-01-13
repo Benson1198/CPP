00e0c74391ba72d33c36a4523afe513bb0e34094
Benson1198 <bensonpx1198@gmail.com>
2021-01-13 12:41:46 +0530

    CF 3

---
+++ Codeforces/327B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+	for(int i=0;i<n;i++){
+		long temp = (3 * n) + i;
+
+		cout << temp << " ";
+	}
+    return 0;
+}
