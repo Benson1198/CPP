b052db75fef6fa790c3aff67cbf6ae9b4e27c31d
Benson1198 <bensonpx1198@gmail.com>
2021-01-14 14:42:05 +0530

    Dijkstras Algo

---
+++ 450 Ques/Graph/Dijkstra’s shortest path.cpp
@@ -0,0 +1,23 @@
+vector<int> dijkstra(vector<int> graph[], int v, int src){
+	vector<int> dist(v,INT_MAX);
+	dist[src] = 0;
+
+	vector<bool> fin(v,false); // Finalised array
+
+	for(int count=0;count<v;count++){
+		u = -1;
+		for(int i=0;i<v;i++){
+			if(!fin[i] && (u==-1 || dist[i] < dist[u])){
+				u=i;
+			}
+		}
+		fin[i] = true;
+	}
+	for(int y =0; y<v ; y++){
+		if(graph[u][y]!=0 && fin[y] == false){
+			dist[y] = min(dist[y], dist[u]+graph[u][y]);
+		}
+	}
+
+	return dist;
+}
